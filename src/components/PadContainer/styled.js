b55d1c45226482408089b9c0f9926e317beadd16
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-12-17 01:15:59 -0300

    Update dist

--- src/components/PadContainer/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const PadContainer = styled.div`
-    padding: 0 calc((100vw - 1440px)/2); 
-    width: 100%;
-`;
--- src/utils/ReturnStatColor.js
+++
@@ -1,9 +0,0 @@
-export const getStatColor = (statValue) => {
-    if (statValue < 50){
-        return "#FF7B2D";
-    } else if (statValue >= 50 && statValue < 100){
-        return "#FFDE6A";
-    } else {
-        return "#8ADC8E";
-    }
-}
